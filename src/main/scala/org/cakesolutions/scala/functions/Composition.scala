36d0b71d25663ab3bdd9a82757afeef7b7ae9e5e
Mario Arias <mario@marioarias.org>
2015-08-19 11:34:45 +0100

    format

--- src/main/scala/org/cakesolutions/scala/functions/Composition.scala
+++ src/main/scala/org/cakesolutions/scala/functions/Composition.scala
@@ -3,20 +3,17 @@ package org.cakesolutions.scala.functions
 
 object Composition {
 
+  private val toUpperCase = (s: String) => s.toUpperCase
 
-  private val toUpperCase = (s:String) => s.toUpperCase
+  private val trim = (s: String) => s.trim
 
-  private val trim = (s:String) => s.trim
+  private val add2 = (i: Int) => i + 2
 
-  private val add2 = (i:Int) => i + 2
-
-  private val multiplyBy5= (i:Int) => i * 5
-
-
+  private val multiplyBy5 = (i: Int) => i * 5
 
   def main(args: Array[String]) {
     val trimUpperCase: (String) => String = trim andThen toUpperCase
-    assert(trimUpperCase(" foo     ")  == "FOO")
+    assert(trimUpperCase(" foo     ") == "FOO")
     val add2multiply5 = add2 andThen multiplyBy5
     assert(add2multiply5(2) == 20)
     // first multiply by 5 then add 2
